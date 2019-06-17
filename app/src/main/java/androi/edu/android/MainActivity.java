406f9cde88eabe1b2401157803dcb59d3cc0b6f1
eliasmerloalvites <elias_exa_20@hotmail.com>
2019-06-17 10:53:18 -0500

    echo

--- app/src/main/java/androi/edu/android/MainActivity.java
+++ app/src/main/java/androi/edu/android/MainActivity.java
@@ -5,7 +5,7 @@ import android.media.AudioManager;
 import android.media.SoundPool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.view.View; 
 
 public class MainActivity extends AppCompatActivity {
 
